2ccd188447bd190793270784ecd7ff8d6d285e2f
MS Venkatesh Varshan <155513707+Varshan2302@users.noreply.github.com>
2026-08-25 14:01:48 +0530

    Add files via upload

---
+++ task_form/task/enum.js
@@ -0,0 +1,24 @@
+const FORM_ENUMS = Object.freeze({
+
+    GENDER: Object.freeze({ 
+        MALE: "Male",
+        FEMALE: "Female",
+        OTHER: "Other" 
+        }),
+
+    STATE: Object.freeze([
+        "Tamil Nadu",
+        "Kerala",
+        "Karnataka",
+        "Andhra Pradesh",
+        "Other"
+    ]),
+
+    COUNTRY: Object.freeze([
+        "India",
+        "USA",
+        "UK",
+        "Other"
+    ])
+
+});
